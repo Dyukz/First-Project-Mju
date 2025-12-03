acf92949796c156b27d6df494ea0b8bc0829cc4c
jannisfps <jannis.flake@online.de>
2025-12-03 15:14:27 +0100

    movement laufen

--- Assets/scrips/Movement.cs
+++ Assets/scrips/Movement.cs
@@ -3,13 +3,12 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {   
     //Movement default settings; Können in evtl. Settings geändert werden dann
-    public string set_forward = "w";
-    public string set_left = "a";
-    public string set_right = "d";
-    public string set_backwards = "s";
-    public string set_jump = "space";
-
-
+    
+    public Settings settings;
+    private float sprint_spd;
+    public float speed;
+    // x rotation ist oben unten y rechts links
+    //
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -19,54 +18,50 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //movement muss ausgeführt werden
-        Forward();
+        speed = 5 * sprint_spd;
+        Move();
     }
 
-    public void Forward()
+
+    void Move()
     {
-        bool move_forward = Input.GetKey(set_forward);
-        //Debug.Log(move_forward);
-        if (move_forward)
+        if (Input.GetKey(settings.set_forward))
         {
             //movement nach vorne
+            transform.position = transform.position + transform.forward * Time.deltaTime * speed;
         }
-    }
-    public void Backwards()
-    {
-        bool move_backwards = Input.GetKey(set_backwards);
-        //Debug.Log(move_backwards);
-        if (move_backwards)
+
+        if (Input.GetKey(settings.set_backwards))
         {
             //movement nach hinten
+            transform.position = transform.position - transform.forward * Time.deltaTime * speed;
         }
-    }
-    public void Left()
-    {
-        bool move_left = Input.GetKey(set_left);
-        //Debug.Log(move_left);
-        if (move_left)
+
+        if (Input.GetKey(settings.set_left))
         {
             //movement nach links
+            transform.position = transform.position - transform.right * Time.deltaTime * speed;
         }
-    }
-    public void Right()
-    
-    {
-        bool move_right = Input.GetKey(set_right);
-        //Debug.Log(move_right);
-        if (move_right)
+
+        if (Input.GetKey(settings.set_right))
         {
             //movement nach rechts
+            transform.position = transform.position + transform.right * Time.deltaTime * speed;
+        }
+
+        if (Input.GetKey(settings.set_sprint))
+        {   //sprint
+            sprint_spd = 2f;  //1.3f; //2f = 2x min 1.0f sonst langsamer
+        }   
+        else 
+        {
+            sprint_spd = 1f; 
+        }
+
+        if (Input.GetKey(settings.set_jump))
+        {
+            //movement springen
         }
     }
-    public void Jump()
-        {
-            bool move_jump = Input.GetKey(set_jump);
-            //Debug.Log(move_jump);
-            if (move_jump)
-            {
-                //movement springen
-            }
-        }
 }   
+//Input.mousePosition
---
+++ Assets/scrips/Settings.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Settings : MonoBehaviour
+{   
+    
+    public KeyCode set_forward = KeyCode.W;
+    public KeyCode set_left = KeyCode.A;
+    public KeyCode set_backwards = KeyCode.S;
+    public KeyCode set_right = KeyCode.D;
+    public KeyCode set_jump = KeyCode.Space;
+    public KeyCode set_sprint = KeyCode.LeftShift;
+    public KeyCode set_interact = KeyCode.E;
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
