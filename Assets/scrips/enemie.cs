0f4b9bdedff4c9f2daf5582ab4eddbee462cae55
Dyukz <dyukzdyukz@gmail.com>
2026-01-07 14:32:26 +0100

    gegner hitbox

--- Assets/scrips/enemie.cs
+++ Assets/scrips/enemie.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class enemie : MonoBehaviour
 {
-    
+    private float coolDown = 0;
     void Start()
     {
         
@@ -12,12 +12,21 @@ public class enemie : MonoBehaviour
     void Update()
     {
         
+ 
+coolDown -= Time.deltaTime;
+        
+            if (coolDown <= 0f)
+            {
+                other.GetComponent <PlayerMain>().playerHealth -= 10;
+            }
+        
     }
     void OnTriggerStay(Collider other)
     {
         if (other.tag == "player")
         {
-            
+           other.GetComponent <PlayerMain>().playerHealth -= 10;
+           coolDown = 1;
         }
     }
 }
