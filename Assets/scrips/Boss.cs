8180beda91a67628f7048486982cb1276125853e
jannisfps <jannis.flake@online.de>
2026-01-21 15:13:02 +0100

    boss kann sich bewegen UBEYD BAU DIE MAP

--- Assets/scrips/Boss.cs
+++ Assets/scrips/Boss.cs
@@ -4,22 +4,38 @@ public class Boss : MonoBehaviour
 {  
     public GameObject waypointParent;
     public float speed = 0.5f;
+
+    private int waypoints = 0;
+    private int temp = 0;
+    private GameObject currentPoint;
     void Start()
     {
-        int waypoints = waypointParent.transform.childCount;
- 
-        for (int i=0; i < waypoints; i++)
-        {  
-            GameObject currentPoint = waypointParent.transform.GetChild(i).gameObject;
-            transform.LookAt(currentPoint.transform);
- 
-            while (difference(currentPoint))
-            {
-                transform.position = transform.forward * speed * Time.deltaTime;
-            }
-        }
+        waypoints = waypointParent.transform.childCount;
+        currentPoint = waypointParent.transform.GetChild(0).gameObject;
+        transform.LookAt(currentPoint.transform);
+        temp = 0;
     }
- 
+
+    void Update()
+    {   
+        if (difference(currentPoint) >= 0.2f)
+        {
+            transform.position += transform.forward * speed * Time.deltaTime;
+        }
+        else NewTarget(temp);
+    }
+
+    void NewTarget(int oldPos)
+    {   
+        if (oldPos == waypoints)
+        {
+            return;
+        }
+        currentPoint = waypointParent.transform.GetChild(oldPos +1).gameObject;
+        transform.LookAt(currentPoint.transform);
+        temp = oldPos +1;
+    }
+
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "player")
@@ -30,9 +46,17 @@ public class Boss : MonoBehaviour
                 return;
             }
  
-            player.playerHealth += -999999;
+            player.playerHealth -= 999999;
         }
     }
- 
+    
+    float difference(GameObject waypoint)
+    {
+        Vector3 pos = transform.position;
+        Vector3 wptPos = waypoint.transform.position;
+
+        float diff = (pos - wptPos).magnitude;
+        return diff;
+    }
     
 }
