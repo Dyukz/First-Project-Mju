63277a5bef120129cde968f3c21e79b653241705
Dyukz <dyukzdyukz@gmail.com>
2026-01-21 14:38:41 +0100

    Update Boss.cs

--- Assets/scrips/Boss.cs
+++ Assets/scrips/Boss.cs
@@ -1,16 +1,38 @@
 using UnityEngine;
-
+ 
 public class Boss : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+{  
+    public GameObject waypointParent;
+    public float speed = 0.5f;
     void Start()
     {
-        
+        int waypoints = waypointParent.transform.childCount;
+ 
+        for (int i=0; i < waypoints; i++)
+        {  
+            GameObject currentPoint = waypointParent.transform.GetChild(i).gameObject;
+            transform.LookAt(currentPoint.transform);
+ 
+            while (difference(currentPoint))
+            {
+                transform.position = transform.forward * speed * Time.deltaTime;
+            }
+        }
     }
-
-    // Update is called once per frame
-    void Update()
+ 
+    void OnTriggerEnter(Collider other)
     {
-        
+        if (other.tag == "player")
+        {
+            PlayerMain player = other.GetComponent<PlayerMain>();
+            if (player == null)
+            {
+                return;
+            }
+ 
+            player.playerHealth += -999999;
+        }
     }
+ 
+    
 }
