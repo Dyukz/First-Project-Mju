d9f472c51883a602c9acde8cc83071a906468132
jannisfps <jannis.flake@online.de>
2026-03-04 14:09:53 +0100

    Update Score.cs

--- Assets/scrips/Score/Score.cs
+++ Assets/scrips/Score/Score.cs
@@ -11,7 +11,6 @@ public class Score : MonoBehaviour
     public void TimeToScore()
     {   
         time = GetComponent<Timer>().time;
-        Debug.Log(time);
         score = 110-(time/12); //140 --> 8min dannach score - min*60/12
         score = Mathf.Clamp(score, 0, 100);
         score = Mathf.Floor(score);
